60d2f4aba47e55f7ba33753208032250e1c76aed
kamalcon <kmconnor@Katies-MacBook-Pro.local>
2017-09-30 13:24:39 +0200

    add jQuery settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@
 $(document).ready(function() {
 	$(".carousel").slick({
     autoplay: true,
-		autoplaySpeed: 4000
+		autoplaySpeed: 4000,
+		arrows: false,
+		draggable: false,
+		pauseOnFocus: false,
+		pauseOnHover: false
   });
 	$("div").css("border", "3px solid red");
 });
